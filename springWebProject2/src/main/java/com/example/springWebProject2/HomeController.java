ebd5b704d6dfd8d309e4a2fa467de6c9b1f6a015
kesava <kesava@DESKTOP-6J9O8UB>
2023-06-21 10:01:50 +0530

    refs #1 springWebProject intial commit

---
+++ springWebProject2/src/main/java/com/example/springWebProject2/HomeController.java
@@ -0,0 +1,17 @@
+
+  package com.example.springWebProject2;
+  
+  import org.springframework.stereotype.Controller; 
+  import org.springframework.web.bind.annotation.RequestMapping; 
+  
+  @Controller 
+  public class HomeController {
+
+
+	  @RequestMapping("home")
+	  public String home() {
+		  System.out.println("home method in controller called"); 
+		  return "home.jsp";
+	  } 
+  }
+ 
---
+++ springWebProject2/src/test/java/com/example/springWebProject2/SpringWebProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springWebProject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
