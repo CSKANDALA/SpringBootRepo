a592610719f999437a0a9b3358ca464c59b3f205
kesava <kesava@DESKTOP-6J9O8UB>
2023-06-22 10:33:35 +0530

    refs #current changes

---
+++ RestWithBoot/src/test/java/com/SpringRest/RestWithBoot/RestWithBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringRest.RestWithBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
